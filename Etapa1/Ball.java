6e9d10c1ee2033e75b7b135ef584c8bc206820e7
Pedro Espinoza <pedropablo.espinoza@gmail.com>
2014-04-29 19:55:06 -0400

    Archivos modificados de la tarea, etapa 1 terminada

--- Etapa1/Ball.java
+++ Etapa1/Ball.java
@@ -1,48 +1,66 @@
 import java.util.*;
+import java.lang.Math.*;
 
 public class Ball extends PhysicsElement {
-   private static int id=0;  // Ball identification number
-   private final double mass;
-   private final double radius;
-   private double pos_t;     // current position at time t
-   private double pos_tPlusDelta;  // next position in delta time in future
-   private double speed_t;   // speed at time t
-   private double speed_tPlusDelta;   // speed in delta time in future
-   
-   private Ball(){   // nobody can create a block without state
-     this(1.0,0.1,0,0);
-   }
-   public Ball(double mass, double radius, double position, double speed){
-      super(id++);
-      this.mass = mass;
-      pos_t = position;
-      speed_t = speed;
-   }
-   public double getRadius() {
-      return radius;
-   }
-   public double getSpeed() {
-      return speed_t;
-   }
-   public void computeNextState(double delta_t, MyWorld world) {
-     Ball b;  // Assumption: on collision we only change speed.   
-     if ((b=world.findCollidingBall(this))!= null){ /* elastic collision */
-        speed_tPlusDelta=(speed_t*(mass-b.getMass())+2*b.getMass()*b.getSpeed())/(mass+b.getMass());
-        pos_tPlusDelta = pos_t;
-     } else {
-        speed_tPlusDelta = speed_t;
-        pos_tPlusDelta = pos_t + speed_t*delta_t;
-     }
-   }
-   public boolean collide(Ball b) {
-   // to be coded by you
-    }
-   public void updateState(){
-     pos_t = pos_tPlusDelta;
-     speed_t = speed_tPlusDelta;
-   }
-   
-   public String getDescription() {
-   // to be coded by you
-   }
+	private static int id=0;  // Ball identification number
+	private final double mass;
+	private final double radius;
+	private double pos_t;     // current position at time t
+	private double pos_tPlusDelta;  // next position in delta time in future
+	private double speed_t;   // speed at time t
+	private double speed_tPlusDelta;   // speed in delta time in future
+
+	private boolean in_collision = false;
+
+	private Ball(){   // nobody can create a block without state
+		this(1.0, 0.1, 0,0);
+	}
+	public Ball(double mass, double radius, double position, double speed){
+		super(id++);
+		this.mass = mass;
+		pos_t = position;
+		speed_t = speed;
+		this.radius = radius;
+	}
+
+	public double getRadius() {
+		return radius;
+	}
+	public double getSpeed() {
+		return speed_t;
+	}
+	public String getState() {
+		return "";
+	}
+	public double getPosition() {
+		return pos_t;
+	}
+	public double getMass() {
+		return this.mass;
+	}
+
+	public void computeNextState(double delta_t, MyWorld world) {
+		Ball b;  // Assumption: on collision we only change speed.   
+		if ((b=world.findCollidingBall(this))!= null && !in_collision){ /* elastic collision */
+			speed_tPlusDelta = (speed_t*(this.mass - b.getMass()) + 2*b.getMass()*b.getSpeed())/(this.mass + b.getMass());
+			in_collision = true;
+			pos_tPlusDelta = pos_t;
+		} else {
+			speed_tPlusDelta = speed_t;
+			pos_tPlusDelta = pos_t + speed_t*delta_t;
+			in_collision = false;
+		}
+	}
+	public boolean collide(Ball b) {
+		if (b == null) return false;
+		return (Math.abs(b.getPosition() - this.pos_t) <= (b.getRadius() + this.radius));
+	}
+	public void updateState(){
+		pos_t = pos_tPlusDelta;
+		speed_t = speed_tPlusDelta;
+	}
+
+	public String getDescription() {
+		return "Ball_" + super.getId();
+	}
 }
--- Etapa1/MyWorld.java
+++ Etapa1/MyWorld.java
@@ -1,50 +1,68 @@
 import java.util.*;
 import java.io.*;
+import java.lang.Math.*;
 
 public class MyWorld {
-   private PrintStream out;
-   
-   private ArrayList<PhysicsElement> elements;  // array to hold everything in my world.
+	private PrintStream out;
 
-   public MyWorld(){
-      this(System.out);  // delta_t= 0.1[ms] and refreshPeriod=200 [ms]
-   }
-   public MyWorld(PrintStream output){
-      out = output;
-      elements = new ArrayList<PhysicsElement>();     
-   }
+	private ArrayList<PhysicsElement> elements;  // array to hold everything in my world.
 
-   public void addElement(PhysicsElement e) {
-      elements.add(e);
-   }
+	public MyWorld(){
+		this(System.out);  // delta_t= 0.1[ms] and refreshPeriod=200 [ms]
+	}
+	public MyWorld(PrintStream output){
+		out = output;
+		elements = new ArrayList<PhysicsElement>();     
+	}
 
-   public void printStateDescription(){
-     String s ="Time\t";
-     for (PhysicsElement e:elements)
-       s+=e.getDescription() + "\t";
-     out.println(s);
-   }
+	public void addElement(PhysicsElement e) {
+		elements.add(e);
+	}
 
-   public void printState(double t){
-    //  to be coded by you
-   }
+	public void printStateDescription(){
+		String s = "Time\t";
+		for (PhysicsElement e: elements)
+		s += e.getDescription() + "\t";
+		out.println(s);
+	}
 
-   public void simulate (double delta_t, double endTime, double samplingTime) {  // simulate passing time
-      double t=0;
-      printStateDescription();
-      printState(t);
-      while (t<endTime) {
-         for(double nextStop=t+samplingTime; t<nextStop; t+=delta_t) {
-           for (PhysicsElement e: elements)   // compute each element next state based on current global state  
-              e.computeNextState(delta_t,this); // compute each element next state based on current global state
-           for (PhysicsElement e: elements)  // for each element update its state. 
-              e.updateState();     // update its state
-         }
-         printState(t);
-      }
-   }   
+	public void printState(double t){
+		String s = "";
+		for (PhysicsElement e: elements) {
+			s += ((Ball)e).getPosition() + ";";
+		}
+		out.println(t + ";" + s);
+	}
 
-   public Ball findCollidingBall(Ball me) {
-      // to be coded by you
-   }  
+	public void simulate (double delta_t, double endTime, double samplingTime) {  // simulate passing time
+		double t = 0;
+		printStateDescription();
+		printState(t);
+		while (t < endTime) {
+			for(double nextStop = t + samplingTime; t < nextStop; t += delta_t) {
+				for (PhysicsElement e: elements)   // compute each element next state based on current global state  
+					if (e instanceof Ball){
+						Ball b = (Ball)e;
+						b.computeNextState(delta_t, this); // compute each element next state based on current global state
+					}
+				for (PhysicsElement e: elements)  // for each element update its state. 
+					if (e instanceof Ball) {
+						Ball b = (Ball)e;
+						b.updateState();     // update its state
+					}
+			}
+			printState(t);
+		}
+	}   
+
+	public Ball findCollidingBall(Ball me) {
+		for (PhysicsElement e: elements) {
+			if (e instanceof Ball) {
+				Ball b = (Ball)e;
+				if (b.getDescription().compareTo(me.getDescription()) != 0)
+					if (b.collide(me)) return b;
+			}
+		}
+		return null;
+	}  
 } 
