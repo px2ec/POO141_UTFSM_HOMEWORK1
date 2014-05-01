b583988cd6a46fd1382c7fa3bdfffe87f253fbd2
Felipe Veas <fjveas@gmail.com>
2014-04-30 20:57:40 -0400

    Script para graficar los csv, requiere de matplotlib. Primer borrador.

---
+++ utils/plot2d.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import sys
+import argparse
+import csv
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def csv_parse(filename):
+    retlist = list()
+    with open(filename, 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        reader.next()
+        for row in reader:
+            retlist.append(map(float, row))
+    return retlist
+
+def main(files):
+    for f in files:
+        plot_values  = csv_parse(f)
+        time = [values[0] for values in plot_values]
+        ball1 = [values[1] for values in plot_values]
+        ball2 = [values[2] for values in plot_values]
+        
+        plt.grid(True)
+        plt.title('Ball 1 (green) | Ball 2 (red)')
+        plt.plot(time, ball1, 'g.-')
+        plt.plot(time, ball2, 'r.-')
+        plt.ylabel('position [m]')
+        plt.xlabel('time [s]')
+
+        plt.show()
+    return 0
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Creates 2-dimensional plots from csv files')
+    parser.add_argument('files', metavar='FILE', type=str, nargs='+')
+    args = parser.parse_args()
+    retval = main(args.files)
+    sys.exit(retval)
