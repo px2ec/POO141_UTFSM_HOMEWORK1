780c6ec49edee0ba441886d0a84248630ef691d3
Felipe Veas <fjveas@gmail.com>
2014-05-02 13:00:06 -0400

    Cambio en el experimento, usando la configuración pedida en el enunciado

--- Etapa2/PhysicsLab.java
+++ Etapa2/PhysicsLab.java
@@ -11,13 +11,11 @@ public class PhysicsLab {
 		MyWorld world = new MyWorld(System.out);
 
 		double mass = 1.0;      //  1.0 [kg]
-		double speed = 0.0;     //  0.0 [m/s]
 		double radius = 0.1;    // 10.0 [cm]
-		double position = 0.0;  //  0.0 [m] 
 		
-		Ball b0 = new Ball(mass, radius, 0, speed);
-		Ball b1 = new Ball(mass, radius, 1.5, 0);
-		Spring s0 = new Spring(1.0, 0.5);
+		Ball b0 = new Ball(mass, radius, 0.0, 0.0);
+		Ball b1 = new Ball(mass, radius, 1.5, 0.0);
+		Spring s0 = new Spring(1.0, 1.0);
 		
 		s0.attachEnd(b0);
 		s0.attachEnd(b1);
